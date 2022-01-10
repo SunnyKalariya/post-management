53fd92195b7d6b0089edea0f8f20ae73db8f21a4
sunny-tatvasoft <88649036+sunny-tatvasoft@users.noreply.github.com>
2022-01-10 12:42:59 +0530

    test commit 2

---
+++ src/models/testModal.ts
@@ -0,0 +1,9 @@
+export interface IEmployeesModal {
+    id: number;
+    name: string;
+    email: string;
+    address: string;
+    password: string;
+    authorization: string;
+    profileImg: string;
+  }
